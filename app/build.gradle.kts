2089ab7f83b8e7ab828efa9502f1ee1f90de31bf
Gab2006 <tostigabriele6@gmail.com>
2025-06-05 15:04:25 +0200

    commit totale

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.android.volley:volley:1.2.1")
+    implementation("com.google.code.gson:gson:2.10.1")
 }
